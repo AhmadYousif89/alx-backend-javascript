1d380551336d3198920ff64c2b165e2da666afbd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-07-24 17:40:32 +0300

    Refactor handleProfileSignup function to return a mapped array of results

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)]);
+  return Promise.allSettled([
+    signUpUser(firstname, lastname),
+    uploadPhoto(filename),
+  ]).then(results => {
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    }));
+  });
 }
