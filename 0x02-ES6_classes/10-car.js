02fba76dd8ab4377123552782df0301b539c0694
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-07-26 23:39:00 +0300

    Add Car class with brand, motor, color properties and cloneCar method

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,36 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  cloneCar() {
+    // Create a new instance using the current instance's constructor
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
