d71794db00e2d5218556e7fd1750e420c771a459
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-07-29 20:48:20 +0300

    Fix endpoint load tracking in queryAPI function

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,9 +4,11 @@ export const weakMap = new WeakMap();
  * @param {{ protocol: 'http', name: 'getUsers' }} endpoint
  */
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, count + 1);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
