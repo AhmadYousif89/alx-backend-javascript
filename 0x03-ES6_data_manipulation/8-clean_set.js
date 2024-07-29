04f55a391619f97f26a79e34b7f7dc3834200662
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-07-29 21:07:39 +0300

    Refactor cleanSet function to use Array.from() instead of spread operator

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  return [...set]
+  return Array.from(set)
     .filter((str) => str.startsWith(startString))
     .map((str) => str.slice(startString.length))
     .join('-');
