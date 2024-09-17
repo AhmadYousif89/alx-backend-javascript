c8ff8e19bc39443eb42a8c5e4d5d69acb8776f5e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-17 23:49:51 +0300

    Refactor countStudents function to use fs module for file operations

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,25 @@
-const readFileSync = require('fs').readFileSync;
+const fs = require('fs');
 
 function countStudents (path) {
-  try {
-    const students = readFileSync(path, 'utf8').split('\n').filter(Boolean).slice(1);
-
-    console.log('Number of students:', students.length);
-
-    const fields = students.reduce((acc, line) => {
-      const [firstName, , , field] = line.split(',');
-      if (!acc[field]) acc[field] = [];
-      acc[field].push(firstName);
-      return acc;
-    }, {});
-
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(
-        `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`
-      );
-    }
-  } catch (err) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+
+  const students = fs.readFileSync(path, 'utf8').split('\n').filter(Boolean).slice(1);
+
+  console.log('Number of students:', students.length);
+
+  const fields = students.reduce((acc, line) => {
+    const [firstName, , , field] = line.split(',');
+    if (!acc[field]) acc[field] = [];
+    acc[field].push(firstName);
+    return acc;
+  }, {});
+
+  for (const [field, names] of Object.entries(fields)) {
+    console.log(
+      `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`
+    );
   }
 }
 
