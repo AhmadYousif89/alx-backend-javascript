733bf5ba0d0d24fd4ab53753823d9eac6dc47efc
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-17 23:21:03 +0300

    Add stdin.js file for user input handling

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+function greeting() {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('data', function (data) {
+    const name = data.toString().trim();
+    name ? console.log('Your name is:', name) : console.log('You did not type anything');
+    if (!process.stdin.isTTY) {
+      console.log('This important software is now closing');
+    }
+    process.exit();
+  });
+}
+
+greeting();
