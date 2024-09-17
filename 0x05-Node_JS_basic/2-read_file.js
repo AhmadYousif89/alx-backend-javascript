a9b4f5823d70c60896b5b43b9921902db4060463
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-17 23:21:07 +0300

    Add countStudents function to read and process student data

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const readFileSync = require('fs').readFileSync;
+
+function countStudents(path) {
+  try {
+    const students = readFileSync(path, 'utf8').split('\n').filter(Boolean).slice(1);
+
+    console.log('Number of students:', students.length);
+
+    const fields = students.reduce((acc, line) => {
+      const [firstName, , , field] = line.split(',');
+      if (!acc[field]) acc[field] = [];
+      acc[field].push(firstName);
+      return acc;
+    }, {});
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
