daf4ae220270d7c90a9358b9c7019d0ceaad6efc
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-18 22:35:05 +0300

    Refactor readDatabase function to improve CSV data parsing

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,26 +1,22 @@
 const fs = require('fs').promises;
 
 /**
- * Reads from a CSV data file.
- * @param {String} csvFile The path to the CSV data file.
- * @returns {Promise<{firstname: String, lastname: String, age: number}[]>}
+ * Read data from a CSV file.
+ * @param {string} csvFile The path to the CSV data file.
+ * @returns {Promise<{firstname: string, lastname: string, age: number}[]>}
  */
 async function readDatabase(csvFile) {
   try {
     const data = await fs.readFile(csvFile, 'utf-8');
-
-    const students = data.trim().split('\n').slice(1).filter(Boolean);
-    const fields = students.reduce((acc, student) => {
+    const students = data.trim().split('\n').slice(1);
+    return students.reduce((acc, student) => {
       const [firstname, lastname, age, field] = student.split(',');
       (acc[field] = acc[field] || []).push({ firstname, lastname, age: Number(age) });
       return acc;
     }, {});
-
-    return fields;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
 
 export default readDatabase;
-module.exports = readDatabase;
