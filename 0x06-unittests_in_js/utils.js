0c46d8b5f9b873ca43b273fa3bbf006b8b86df8d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-19 18:09:55 +0300

    Refactor calculateNumber function in utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,15 +1,22 @@
 const Utils = {
+  /**
+   * Calculate the result of a math operation between two numbers
+   * @param {'SUM'|'SUBTRACT'|'DIVIDE'} type: the type of operation to perform
+   * @param {number} a: The first number
+   * @param {number} b: The second number
+   * @returns: The result of the operation or 'Error' if the operation is invalid
+   */
   calculateNumber(type, a, b) {
-    if (type === 'SUM') {
-      return Math.round(a) + Math.round(b);
-    }
-    if (type === 'SUBTRACT') {
-      return Math.round(a) - Math.round(b);
-    }
-    if (type === 'DIVIDE') {
-      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-    }
-    return 0;
+    const operations = {
+      SUM: (a, b) => Math.round(a) + Math.round(b),
+      SUBTRACT: (a, b) => Math.round(a) - Math.round(b),
+      DIVIDE: (a, b) => {
+        if (Math.round(b) === 0) return 'Error';
+        return Math.round(a) / Math.round(b);
+      },
+    };
+
+    return operations[type] ? operations[type](a, b) : 'Opertation type is not valid';
   },
 };
 
