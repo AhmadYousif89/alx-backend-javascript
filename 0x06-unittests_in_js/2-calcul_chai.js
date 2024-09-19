0ba79e4b6517f97abf269f7457af7d522adb2db9
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-19 16:12:45 +0300

    Add calculateNumber function for math operations with error handling

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,21 @@
+/**
+ * Calculate the result of a math operation between two numbers
+ * @param {'SUM'|'SUBTRACT'|'DIVIDE'} type: the type of operation to perform
+ * @param {number} a: The first number
+ * @param {number} b: The second number
+ * @returns: The result of the operation or 'Error' if the operation is invalid
+ */
+function calculateNumber(type, a, b) {
+  const operations = {
+    SUM: (a, b) => Math.round(a) + Math.round(b),
+    SUBTRACT: (a, b) => Math.round(a) - Math.round(b),
+    DIVIDE: (a, b) => {
+      if (Math.round(b) === 0) return 'Error';
+      return Math.round(a) / Math.round(b);
+    },
+  };
+
+  return operations[type] ? operations[type](a, b) : 'Opertation type is not valid';
+}
+
+module.exports = calculateNumber;
