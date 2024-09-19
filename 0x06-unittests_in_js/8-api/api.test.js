3375971b93f808e157e657cd4034ce5bb4b8ff21
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-19 20:16:57 +0300

    Remove unnecessary before and after hooks in API test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,16 +4,6 @@ const { expect } = require('chai');
 describe('GET /', () => {
   const url = 'http://localhost:7865';
 
-  before((done) => {
-    require('./api.js');
-    done();
-  });
-
-  after((done) => {
-    done();
-    process.exit();
-  });
-
   it('Check the code and the body', (done) => {
     request.get(url, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
