96681ab9b40c508805284a8d72f2afc9f46fe4ef
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-19 18:41:28 +0300

    Add spies for calculateNumber and console.log

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,12 +6,15 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('Test sendPaymentRequestToApi', () => {
   let calculateNumberSpy;
+  let consoleLogSpy;
 
   beforeEach(() => {
     calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    consoleLogSpy = sinon.spy(console, 'log');
   });
   afterEach(() => {
     calculateNumberSpy.restore();
+    consoleLogSpy.restore();
   });
 
   // Test the function call to Utils.calculateNumber using spies
@@ -19,38 +22,37 @@ describe('Test sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber with correct arguments', () => {
     sendPaymentRequestToApi(80, 20);
     expect(calculateNumberSpy.calledOnceWithExactly('SUM', 80, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 100')).to.be.true;
   });
 
   it('should handle decimal numbers correctly', () => {
     sendPaymentRequestToApi(10.7, 4.3);
     expect(calculateNumberSpy.calledOnceWithExactly('SUM', 10.7, 4.3)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 15')).to.be.true;
   });
 
   it('should handle negative numbers correctly', () => {
     sendPaymentRequestToApi(-10, -20);
     expect(calculateNumberSpy.calledOnceWithExactly('SUM', -10, -20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: -30')).to.be.true;
   });
 
   // Test the function result
   it('should return the correct total amount', () => {
     const result = sendPaymentRequestToApi(80, 20);
     expect(result).to.equal(100);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 100')).to.be.true;
   });
 
   it('should round the result correctly', () => {
     const result = sendPaymentRequestToApi(10.2, 4.8);
     expect(result).to.equal(15);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 15')).to.be.true;
   });
 
   it('should handle negative numbers', () => {
     const result = sendPaymentRequestToApi(-50, 20);
     expect(result).to.equal(-30);
-  });
-
-  // Test with invalid type
-
-  it('should throw an error if the type is invalid', () => {
-    sendPaymentRequestToApi(80, 20);
-    expect(calculateNumberSpy.calledOnceWithExactly('INVALID', 80, 20)).to.be.false;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: -30')).to.be.true;
   });
 });
