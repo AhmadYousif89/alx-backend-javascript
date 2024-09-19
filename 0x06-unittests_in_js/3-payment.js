91bafe236eda6f4740c4933f5a85fb62fba5fedd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-19 17:32:07 +0300

    Remove unnecessary return statement

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -8,7 +8,6 @@ const calculateNumber = require('./utils').calculateNumber;
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
-  return total;
 }
 
 module.exports = sendPaymentRequestToApi;
